3acf584f0b0319024e086edb82c410efe5e61045
jgoodson2 <goodson.jonathon@gmail.com>
2015-01-29 21:46:02 -0500

    wrote signatures for the five js methods in app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 //Solution: Add interactivity so the user can amanage daily tasks
 
 //add a task
+var addTask = function(){
   //when the button is pressed, we want to create a task
   //create a new list item with the text from the new task
   //input (checkbox)
@@ -10,8 +11,10 @@
   //button.edit
   //button.delete
   //each elements, needs modified and appended
+}
   
 //edit a task
+var editTask = function(){
   //when the edit button is pressed
     //if the class of the parent is .editMode
       //switch from .editMode
@@ -21,15 +24,22 @@
       //input value becomes label's text
       
     //toggle .editMode on the parent
+}
 
+var deleteTask = function() {
 //delete existing task
   //when the delete button is pressed
     //remove the parent list item from the unordered list
+    }
 
 //mark a task as complete
+var taskComplete = function() {
   //when the checkbox is checked
     //append the task list item #completed-tasks
-
+    }
+    
 //mark a task as incomplete
+var taskIncomplete = function(){
   //when the checkbox is unchecked
     //append to #incomplete-tasks
+    }
