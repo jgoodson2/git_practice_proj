1520f36c958e5bbc271626ab1c1ace89c8a67fbe
jgoodson2 <goodson.jonathon@gmail.com>
2015-01-29 21:39:31 -0500

    planned out javascript in pseudocode

--- js/app.js
+++ js/app.js
@@ -1,2 +1,35 @@
 //Problem: user interaction doesnt provide desired results
 //Solution: Add interactivity so the user can amanage daily tasks
+
+//add a task
+  //when the button is pressed, we want to create a task
+  //create a new list item with the text from the new task
+  //input (checkbox)
+  //label
+  //input (text) 
+  //button.edit
+  //button.delete
+  //each elements, needs modified and appended
+  
+//edit a task
+  //when the edit button is pressed
+    //if the class of the parent is .editMode
+      //switch from .editMode
+      //label text becomes input's value
+    //else
+      //switch to .editMode
+      //input value becomes label's text
+      
+    //toggle .editMode on the parent
+
+//delete existing task
+  //when the delete button is pressed
+    //remove the parent list item from the unordered list
+
+//mark a task as complete
+  //when the checkbox is checked
+    //append the task list item #completed-tasks
+
+//mark a task as incomplete
+  //when the checkbox is unchecked
+    //append to #incomplete-tasks
